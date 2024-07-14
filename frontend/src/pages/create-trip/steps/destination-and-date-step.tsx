e21018c748f0e8d4234d1cb4ddc6827491087ee9
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-07-14 15:44:19 -0300

    fix: page create-trip (tela inicial)

---
+++ frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -0,0 +1,43 @@
+import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
+
+interface DestinationAndDateStepProps {
+    isGuestInputOpen: boolean
+    closeGuestsInput: () => void
+    openGuestsInput: () => void
+}
+
+export function DestinationAndDateStep(
+    { isGuestInputOpen, closeGuestsInput, openGuestsInput }: DestinationAndDateStepProps
+) {
+    return (
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <div className='flex items-center gap-2 flex-1'>
+                <MapPin className="size-5 text-zinc-400" />
+                <input disabled={isGuestInputOpen} type="text" placeholder=" Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 out disabled={isGuestInputOpen}line-none" />
+            </div>
+
+            <div className='flex items-center gap-2 '>
+                <Calendar className="size-5 text-zinc-400" />
+                <input type="text" placeholder=" Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+            </div>
+
+            <div className='w-px h-6 bg-zinc-800' />
+
+
+
+            {isGuestInputOpen ? (
+                <Button onClick={closeGuestsInput} variant="secondary">
+                    Alterar local/data
+                    <Settings2 className='size-5'></Settings2>
+                </Button>
+            ) : (
+                <Button onClick={openGuestsInput} variant="primary">
+                    Continuar
+                    <ArrowRight className='size-5' />
+                </Button>
+            )}
+        </div>
+    )
+
+}
